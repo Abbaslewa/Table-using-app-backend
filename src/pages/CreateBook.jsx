141133d475d4776d759e3a3c5fee0ade1c9916ba
AbbasLewa <abbasomerhassn@gmail.com>
2025-02-24 12:47:23 +0200

    catch

--- src/pages/CreateBook.jsx
+++ src/pages/CreateBook.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
@@ -29,50 +29,51 @@ const CreateBooks = () => {
       })
       .catch((error) => {
         setLoading(false);
+        // alert('An error happened. Please Chack console');
         enqueueSnackbar('Error', { variant: 'error' });
         console.log(error);
       });
   };
 
   return (
-    <div className='p-4'>
+    <div className='p-5'>
       <BackButton />
-      <h1 className='text-4xl my-6 text-blue-950'>Create Book</h1>
+      <h1 className='text-3xl my-4'>Create Book</h1>
       {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-blue-950 rounded-xl w-[600px] p-6 mx-auto bg-gradient-to-r from-blue-100 to-blue-50'>
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-blue-950'>Title</label>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
           <input
             type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-blue-950 px-4 py-2 w-full rounded-lg text-blue-950'
+            className='border-2 border-gray-500 px-4 py-2 w-full'
           />
         </div>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-blue-950'>Author</label>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
           <input
             type='text'
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-blue-950 px-4 py-2 w-full rounded-lg text-blue-950'
+            className='border-2 border-gray-500 px-4 py-2  w-full '
           />
         </div>
         <div className='my-4'>
-          <label className='text-xl mr-4 text-blue-950'>Publish Year</label>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
           <input
             type='number'
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-blue-950 px-4 py-2 w-full rounded-lg text-blue-900'
+            className='border-2 border-gray-500 px-4 py-2  w-full '
           />
         </div>
-        <button className='p-3 bg-blue-950 text-white rounded-lg hover:bg-blue-900 transition-all duration-200' onClick={handleSaveBook}>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
           Save
         </button>
       </div>
     </div>
   );
-};
+}
 
-export default CreateBooks;
+export default CreateBooks
